59a7733da1326ccd1bb8f0dc714919ec78f512bc
daeunllee <daeun.lee.dana@gmail.com>
2026-09-22 20:52:44 -0400

    deleted TODO msg

--- exercises/ex02-first-letters/src/main/java/FirstLetters.java
+++ exercises/ex02-first-letters/src/main/java/FirstLetters.java
@@ -25,7 +25,6 @@ public class FirstLetters {
      * @return the first character of each word, concatenated
      */
     public static String firstLetters(String words) {
-        // TODO: complete
         String fL = new String();
 
         for (int i = 0; i < words.length(); i++) {
