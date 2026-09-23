c8ac312309051021faeae549c10819f03e3ab8f2
daeunllee <daeun.lee.dana@gmail.com>
2026-09-22 21:31:14 -0400

    removed TODO & implemented body

--- exercises/ex09-instance-counter/src/main/java/Robot.java
+++ exercises/ex09-instance-counter/src/main/java/Robot.java
@@ -27,10 +27,9 @@ public class Robot {
    * @param name this Robot's name
    */
   public Robot(String name) {
-    // TODO: set this.name; set this.id to the current value of count (so the
-    //       first Robot gets id 0); then increase count by 1.
-    this.id = 0;
-    this.name = null;
+    this.id = count;
+    count++;
+    this.name = name;
   }
 
   /**
